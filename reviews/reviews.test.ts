e1761f36f85f97c84711e4de25d68fa08d30e87b
Luiz Gustavo Xavier <luizxvr@hotmail.com>
2020-01-16 20:30:24 -0300

    refactoring de inicializacao de testes

---
+++ reviews/reviews.test.ts
@@ -0,0 +1,15 @@
+import 'jest'
+import * as request from 'supertest'
+import { Server } from '../server/server'
+import { environment } from '../common/environment'
+
+let address: string = 'http://localhost:3001'
+
+test('get /reviews', () => {
+    return request(address)
+        .get('/reviews')
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body.items).toBeInstanceOf(Array)
+        }).catch(fail)
+})
